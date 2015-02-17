bc02f945621013a93a3b459f1064db8c40f7f86a
Dan Stepanov <danhstepanov@gmail.com>
2015-02-16 21:00:02 -0500

    Initial Commit

---
+++ SampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleApp
+//
+//  Created by Daniel Stepanov on 2/16/15.
+//  Copyright (c) 2015 Daniel Stepanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Daniel Stepanov on 2/16/15.
+//  Copyright (c) 2015 Daniel Stepanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
